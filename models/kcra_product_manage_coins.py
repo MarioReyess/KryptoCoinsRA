916f962c525828950542ba2138efa0e87da557c6
Mario <Mario@DESKTOP-0COEC4U>
2019-05-07 17:18:04 +0200

    Fixed Views, Tags

--- models/kcra_product_manage_coins.py
+++ models/kcra_product_manage_coins.py
@@ -4,8 +4,8 @@ from odoo import models, fields, api
 
 
 class kcra__product_manage(models.Model):
-    _name = 'kcra__product_manage.kcra__product_manage_coins'
+    _name = 'kcra__product_manage_coins'
 
     name = fields.Char(string="Nom moneda")
     price = fields.Float(string="Preu")
-    tags_id = fields.Many2one("kcra__product_manage_tags", string="Etiqueta")
+    tags_id = fields.Many2many("kcra__product_manage_tags", string="Etiqueta")
--- models/kcra_product_manage_tags.py
+++ models/kcra_product_manage_tags.py
@@ -3,9 +3,8 @@
 from odoo import models, fields, api
 
 
-class kcra__product_manage(models.Model):
-    _name = 'kcra__product_manage.kcra__product_manage_tags'
+class kcra__product_manage_tags(models.Model):
+    _name = 'kcra__product_manage_tags'
 
     name = fields.Char(string="Nom etiqueta")
-    coins_id = fields.Many2one("kcra__product_manage_coins", string="Coins")
-
+    # coins_id = fields.One2many("kcra__product_manage_coins", "tags_id", string="Coins")
